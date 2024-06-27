0a0433b193a4f302f00e91e4c2e69d34f565bee3
dioneuty <1@293f3900>
2024-06-27 22:22:16 +0900

    Chapter 9. Installing Next.js (npm install react react-dom next)

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import {useState} from 'react';
+
+function Header({title}) {
+    return <h1>{title ? title : 'Default Title'}</h1>;
+}
+
+export default function HomePage() {
+    const names = ['홍길동', '이순신', '장보고'];
+
+    const [likes, setLikes] = React.useState(0);
+
+    function handleClick() {
+        setLikes(likes + 1);
+    }
+
+    return (
+        <div>                        
+            <Header title="Hello World" /> {/* Header 컴포넌트를 중첩함 */}
+            <Header title="Hello React" />
+            <Header />
+            <ul>
+                {names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+            <button onClick={handleClick}>Like ({likes})</button>
+        </div>
+    );
+}
